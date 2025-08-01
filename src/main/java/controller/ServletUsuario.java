133043bfdab2c85e1a9a18f755ac918efe5a0360
breyes396 <breyes-2024396@kinal.edu.gt>
2025-08-01 14:30:57 -0600

    Se crea servlets para mantenimiento usuario

---
+++ src/main/java/controller/ServletUsuario.java
@@ -0,0 +1,39 @@
+package controller;
+
+import dao.UsuarioDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Usuario;
+
+@WebServlet("/ServletUsuario")
+public class ServletUsuario extends HttpServlet{
+    
+    private UsuarioDAO usuarioDao = new UsuarioDAO();
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+//        try {
+//            Usuario usuario = new Usuario();
+//            usuario.setNombre(request.getParameter("nombre"));
+//            usuario.setApellido(request.getParameter("apellido"));
+//            usuario.setTelefono(request.getParameter("telefono"));
+//            usuario.setCorreo(request.getParameter("correo"));
+//            usuario.setDireccion(request.getParameter("direccion"));
+//            usuario.setGenero(request.getParameter("genero"));
+//            usuario.setRol(request.getParameter("rol"));
+//            usuario.setContrasena(request.getParameter("contrasena"));
+//            
+//            usuarioDao.guardar(usuario);
+//            response.sendRedirect("ServletListarUsuario");
+//        } catch (SQLException e) {
+//            throw new ServletException("Error JDBC al guardar", e);
+//        }
+    }
+}
